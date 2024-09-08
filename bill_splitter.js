306855c6843e62bb68066ed99992672c34bf6222
Milan Ranpariya <ranpariya04@gmail.com>
2024-09-08 19:27:54 -0400

    Create and test calculateTip function

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,11 +1,22 @@
 // Task 1: Calculate the Tip
 
 // Define bill and calculate tip based on bill amount using ternary operator
-let bill = 275
+let bill = 275;
 let tip = (bill >= 50 && bill <= 300) ? (bill * 0.15) : (bill * 0.20);
 
 // Task 2: Output bill, tip, and total
 
-//Define total, then output the message with the bill, tip, and total.
-let total = bill + tip
-console.log(`The bill was $${bill}, the tip was $${tip}, and the total was $${total}`)
+// Define total, then output the message with the bill, tip, and total.
+let total = bill + tip;
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total was $${total}`);
+
+// Task 3: Create calculateTip function
+
+// Create a function to calculate the tip of any bill
+function calculateTip(bill) {
+    return (bill >=50 && bill <= 300) ? (bill * 0.15) : (bill * 0.20);
+}
+
+let testBill = 100;
+let testTip = calculateTip(testBill);
+console.log(`The tip for a bill of $${testBill} is $${testTip}`);
